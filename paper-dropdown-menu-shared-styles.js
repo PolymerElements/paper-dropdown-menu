16ea1f33b0962fe8f48a281b6d38d3e59271a525
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:39 -0700

    auto-converted by polymer-modulizer

--- paper-dropdown-menu-shared-styles.js
+++ paper-dropdown-menu-shared-styles.js
@@ -1,16 +1,8 @@
-<!--
-@license
-Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+import '@polymer/paper-styles/default-theme.js';
+const $_documentContainer = document.createElement('template');
+$_documentContainer.setAttribute('style', 'display: none;');
 
-<link rel="import" href="../paper-styles/default-theme.html">
-
-<dom-module id="paper-dropdown-menu-shared-styles">
+$_documentContainer.innerHTML = `<dom-module id="paper-dropdown-menu-shared-styles">
   <template>
     <style>
       :host {
@@ -19,7 +11,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         text-align: left;
 
         /* NOTE(cdata): Both values are needed, since some phones require the
-         * value to be `transparent`.
+         * value to be \`transparent\`.
          */
         -webkit-tap-highlight-color: rgba(0,0,0,0);
         -webkit-tap-highlight-color: transparent;
@@ -75,4 +67,22 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       }
     </style>
   </template>
-</dom-module>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer.content);
+
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;
